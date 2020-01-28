b876d9933e419450391e3fd157e52ae4da683b7f
tgpx31 <brianmichaelbaron@gmail.com>
2020-01-27 21:32:02 -0500

    stubbing out portfolio data and setting up some basics

--- _tailwind/tailwind.config.js
+++ _tailwind/tailwind.config.js
@@ -1,6 +1,101 @@
-// tailwind.config.js
 module.exports = {
-    theme: {},
-    variants: {},
+    prefix: '',
+    important: false,
+    separator: ':',
+    theme: {
+      screens: {
+        sm: '550px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        print: {'raw':'print'},
+      },
+      extend: {
+        colors: {
+          red: {
+            100: '#fff0f0',
+            200: '#feebc8',
+            300: '#fec8c8',
+            400: '#fd3434',
+            500: '#ed3636',
+            600: '#df4646',
+            700: '#f15d2a',
+            800: '#f12a2a',
+            900: '#7b1e1e',
+          },
+          black: {
+            900: "#000000",
+          },
+        },
+        listStyleType: {
+          circle: 'circle',
+        },
+        fontSize: {
+          prlg: '1.4rem'
+        },
+      },
+      fontFamily: {
+        sans: [ '"Roboto Condensed"', '"Trebuchet MS"', 'Arial', 'Helvetica', 'sans-serif'],
+        serif: [
+          '"Crimson Text"',
+          'Georgia',
+          'Cambria',
+          '"Times New Roman"',
+          'Times',
+          'serif',
+        ],
+        mono: [
+          'Menlo',
+          'Monaco',
+          'Consolas',
+          '"Liberation Mono"',
+          '"Courier New"',
+          'monospace',
+        ],
+        head: [ '"Roboto Condensed"', '"Trebuchet MS"', 'Arial', 'Helvetica', 'sans-serif'],
+        headlite: [ '"Roboto"', '"Trebuchet MS"', 'Arial', 'Helvetica', 'sans-serif' ],
+        body: [ '"Crimson Text"']
+      },
+      height: theme => ({
+        auto: 'auto',
+        ...theme('spacing'),
+        full: '100%',
+        screen: '100vh',
+        sm: '10rem',
+        md: '12rem',
+        lg: '14rem',
+        xl: '16rem',
+        '2xl': '20rem',
+        '3xl': '24rem',
+        '4xl': '28rem',
+        '5xl': '32rem',
+        '6xl': '40rem',
+        '7xl': '60rem',
+        '8xl': '72rem',
+      }),
+      maxHeight: theme => ({
+        full: '100%',
+        screen: '100vh',
+        ...theme('height'),
+      }),
+      maxWidth: {
+        xs: '20rem',
+        sm: '24rem',
+        md: '28rem',
+        lg: '32rem',
+        xl: '36rem',
+        '2xl': '42rem',
+        '3xl': '48rem',
+        '4xl': '56rem',
+        '5xl': '64rem',
+        '6xl': '72rem',
+        full: '100%',
+        1: '1rem',
+        2: '2rem',
+        3: '3rem',
+        4: '4rem',
+      },
+    },
+    corePlugins: {},
     plugins: [],
-}
+  }
